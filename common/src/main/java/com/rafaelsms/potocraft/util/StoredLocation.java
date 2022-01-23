bae5a0186a367f6d9b4a13bd55799910133f3d2e
Rafael S. M. Santos <1348868+rafaelsms@users.noreply.github.com>
2022-01-23 07:14:54 -0300

    Add yaw, pitch support for location

--- common/src/main/java/com/rafaelsms/potocraft/util/StoredLocation.java
+++ common/src/main/java/com/rafaelsms/potocraft/util/StoredLocation.java
@@ -12,6 +12,7 @@ public class StoredLocation extends DatabaseObject {
 
     private final @NotNull UUID worldId;
     private final double x, y, z;
+    private final float yaw, pitch;
 
     public StoredLocation(@NotNull Location location) {
         assert location.getWorld() != null;
@@ -19,6 +20,8 @@ public class StoredLocation extends DatabaseObject {
         this.x = location.getX();
         this.y = location.getY();
         this.z = location.getZ();
+        this.yaw = location.getYaw();
+        this.pitch = location.getPitch();
     }
 
     public StoredLocation(@NotNull Document document) {
@@ -27,10 +30,13 @@ public class StoredLocation extends DatabaseObject {
         this.x = document.getDouble(Keys.X_KEY);
         this.y = document.getDouble(Keys.Y_KEY);
         this.z = document.getDouble(Keys.Z_KEY);
+        this.yaw = Util.getOrElse(Util.convert(document.getString(Keys.YAW_KEY), Float::parseFloat), 0.0f);
+        this.pitch = Util.getOrElse(Util.convert(document.getString(Keys.PITCH_KEY), Float::parseFloat), 0.0f);
+        ;
     }
 
     public @NotNull Location toLocation(@NotNull Plugin plugin) {
-        return new Location(plugin.getServer().getWorld(worldId), x, y, z);
+        return new Location(plugin.getServer().getWorld(worldId), x, y, z, yaw, pitch);
     }
 
     @Override
@@ -40,6 +46,8 @@ public class StoredLocation extends DatabaseObject {
         document.put(Keys.X_KEY, x);
         document.put(Keys.Y_KEY, y);
         document.put(Keys.Z_KEY, z);
+        document.put(Keys.YAW_KEY, String.valueOf(yaw));
+        document.put(Keys.PITCH_KEY, String.valueOf(pitch));
         return document;
     }
 
@@ -49,6 +57,8 @@ public class StoredLocation extends DatabaseObject {
         public static final String X_KEY = "x";
         public static final String Y_KEY = "y";
         public static final String Z_KEY = "z";
+        public static final String YAW_KEY = "yaw";
+        public static final String PITCH_KEY = "pitch";
 
         // Private constructor
         private Keys() {
