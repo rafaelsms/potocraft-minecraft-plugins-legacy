c17a05b1423703e8e98b4a1ead4a14379534fb3c
Rafael S. M. Santos <1348868+rafaelsms@users.noreply.github.com>
2022-01-30 08:22:48 -0300

    Fix logging locations to use event-oriented location

--- ServerUtility/src/main/java/com/rafaelsms/potocraft/serverutility/listeners/PlayerLogging.java
+++ ServerUtility/src/main/java/com/rafaelsms/potocraft/serverutility/listeners/PlayerLogging.java
@@ -5,8 +5,10 @@ import com.destroystokyo.paper.event.player.PlayerStartSpectatingEntityEvent;
 import com.destroystokyo.paper.event.player.PlayerStopSpectatingEntityEvent;
 import com.rafaelsms.potocraft.serverutility.ServerUtilityPlugin;
 import com.rafaelsms.potocraft.util.Util;
+import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerPortalEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
@@ -22,49 +24,53 @@ public class PlayerLogging implements Listener {
         this.plugin = plugin;
     }
 
-    @EventHandler(ignoreCancelled = true)
+    @EventHandler(ignoreCancelled = true, priority = EventPriority.MONITOR)
     private void log(PlayerElytraBoostEvent event) {
-        logPlayer(event.getPlayer(), "elytra boosted");
+        logPlayer(event.getPlayer(), event.getPlayer().getLocation(), "elytra boosted");
     }
 
-    @EventHandler(ignoreCancelled = true)
+    @EventHandler(ignoreCancelled = true, priority = EventPriority.MONITOR)
     private void log(PlayerTeleportEvent event) {
-        logPlayer(event.getPlayer(), "teleported by %s".formatted(event.getCause().name().toLowerCase()));
+        logPlayer(event.getPlayer(),
+                  event.getTo(),
+                  "teleported by %s".formatted(event.getCause().name().toLowerCase()));
     }
 
-    @EventHandler(ignoreCancelled = true)
+    @EventHandler(ignoreCancelled = true, priority = EventPriority.MONITOR)
     private void log(PlayerQuitEvent event) {
-        logPlayer(event.getPlayer(), "quit");
+        logPlayer(event.getPlayer(), event.getPlayer().getLocation(), "quit");
     }
 
-    @EventHandler(ignoreCancelled = true)
+    @EventHandler(ignoreCancelled = true, priority = EventPriority.MONITOR)
     private void log(PlayerPortalEvent event) {
-        logPlayer(event.getPlayer(), "entered portal");
+        logPlayer(event.getPlayer(), event.getTo(), "entered portal");
     }
 
-    @EventHandler(ignoreCancelled = true)
+    @EventHandler(ignoreCancelled = true, priority = EventPriority.MONITOR)
     private void log(PlayerStartSpectatingEntityEvent event) {
         if (event.getNewSpectatorTarget() instanceof Player player) {
-            logPlayer(event.getPlayer(), "spectating %s".formatted(player.getName()));
+            logPlayer(event.getPlayer(), player.getLocation(), "spectating %s".formatted(player.getName()));
         }
     }
 
-    @EventHandler(ignoreCancelled = true)
+    @EventHandler(ignoreCancelled = true, priority = EventPriority.MONITOR)
     private void log(PlayerStopSpectatingEntityEvent event) {
         if (event.getSpectatorTarget() instanceof Player player) {
-            logPlayer(event.getPlayer(), "stopped spectating %s".formatted(player.getName()));
+            logPlayer(event.getPlayer(),
+                      event.getPlayer().getLocation(),
+                      "stopped spectating %s".formatted(player.getName()));
         }
     }
 
-    private void logPlayer(@NotNull Player player, @Nullable String action) {
+    private void logPlayer(@NotNull Player player, @NotNull Location location, @Nullable String action) {
         if (plugin.getConfiguration().isPlayerLoggingEnabled()) {
             plugin.logger()
                   .info("%s %s at world = %s, %d %d %d".formatted(player.getName(),
                                                                   Util.getOrElse(action, ""),
                                                                   player.getWorld().getName(),
-                                                                  player.getLocation().getBlockX(),
-                                                                  player.getLocation().getBlockY(),
-                                                                  player.getLocation().getBlockZ()));
+                                                                  location.getBlockX(),
+                                                                  location.getBlockY(),
+                                                                  location.getBlockZ()));
         }
     }
 }
