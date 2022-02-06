c60fcab04eb6dadc821c590c3bdc81347956469d
Rafael S. M. Santos <1348868+rafaelsms@users.noreply.github.com>
2022-02-06 11:13:02 -0300

    Fix player item mend unexpected behaviour

--- ServerUtility/src/main/java/com/rafaelsms/potocraft/serverutility/listeners/MendingNerfListener.java
+++ ServerUtility/src/main/java/com/rafaelsms/potocraft/serverutility/listeners/MendingNerfListener.java
@@ -7,8 +7,11 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerItemMendEvent;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.HashSet;
+
 public class MendingNerfListener implements Listener {
 
+    private final HashSet<Integer> experienceOrbId = new HashSet<>();
     private final @NotNull ServerUtilityPlugin plugin;
 
     public MendingNerfListener(@NotNull ServerUtilityPlugin plugin) {
@@ -22,8 +25,16 @@ public class MendingNerfListener implements Listener {
         }
         // The default formula: half is going to the repair (which repairs double the amount) and half is going to the player
         // What we will do: half will be consumed to repair 1 point only and half will go to the player
+        // TODO: remove workaround for https://github.com/PaperMC/Paper/issues/7449
+        if (experienceOrbId.contains(event.getExperienceOrb().getEntityId())) {
+            return;
+        }
+        experienceOrbId.add(event.getExperienceOrb().getEntityId());
         int halfExperience = event.getExperienceOrb().getExperience() / 2;
         event.getExperienceOrb().setExperience(halfExperience);
         event.setRepairAmount(Math.min(event.getRepairAmount(), 1));
+        plugin.getServer()
+              .getScheduler()
+              .runTaskLater(plugin, () -> experienceOrbId.remove(event.getExperienceOrb().getEntityId()), 20);
     }
 }
