438425486c88870e5295eef3416ad81ba0a0547a
Rafael S. M. Santos <1348868+rafaelsms@users.noreply.github.com>
2022-04-18 06:13:32 -0300

    Fix server redirect on /login

--- LoginManager/src/main/java/com/rafaelsms/potocraft/loginmanager/commands/LoginCommand.java
+++ LoginManager/src/main/java/com/rafaelsms/potocraft/loginmanager/commands/LoginCommand.java
@@ -6,9 +6,7 @@ import com.rafaelsms.potocraft.loginmanager.Permissions;
 import com.rafaelsms.potocraft.loginmanager.player.Profile;
 import com.rafaelsms.potocraft.loginmanager.util.LoginUtil;
 import net.md_5.bungee.api.CommandSender;
-import net.md_5.bungee.api.config.ServerInfo;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
-import net.md_5.bungee.api.event.ServerConnectEvent;
 import net.md_5.bungee.api.plugin.Command;
 import net.md_5.bungee.api.plugin.TabExecutor;
 import org.jetbrains.annotations.NotNull;
@@ -92,27 +90,14 @@ public class LoginCommand extends Command implements TabExecutor {
             player.disconnect(plugin.getConfiguration().getKickMessageFailedToSaveProfile());
         }
 
-        // Attempt to send player to last connected server
-        if (profile.getLastServerName().isPresent()) {
-            ServerInfo serverInfo = plugin.getProxy().getServerInfo(profile.getLastServerName().get());
-            if (serverInfo != null) {
-                player.connect(serverInfo, (result, error) -> {
-                    // Fallback to default server
-                    if (error != null) {
-                        LoginUtil.sendPlayerToDefaultServer(plugin, player);
-                    }
-                }, ServerConnectEvent.Reason.PLUGIN);
-                return;
-            }
-        }
-        // If there isn't a last connected server, send to default one
+        // Send player to default server
         LoginUtil.sendPlayerToDefaultServer(plugin, player);
     }
 
     @Override
     public Iterable<String> onTabComplete(CommandSender sender, String[] args) {
         if (args.length <= 1) {
-            return List.of("123456", "098765");
+            return List.of("123456", "senhaSegura");
         }
         return List.of();
     }
